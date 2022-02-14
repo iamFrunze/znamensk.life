2b96c8702acf9d0319a4c834068d8add11436695
iamFrunze <byfrunze@gmail.com>
2022-02-14 19:35:53 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# znamensk
+
+Znamensk.Life
+
+Created at 14.02.2022 18:30 (MSK) by Anton Korotkov
+Contact email: byfrunze@gmail.com
+
---
+++ android/app/src/main/kotlin/com/byfrunze/znamensk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.byfrunze.znamensk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
